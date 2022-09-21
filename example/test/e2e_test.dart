b7027c67177a896acd4746f51dfbf838b5474a5c
Gabriel Terwesten <gabriel@terwesten.net>
2022-09-21 22:53:13 +0200

    test: add basic E2E test

--- example/test/e2e_test.dart
+++ example/test/e2e_test.dart
@@ -1,5 +1,89 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:test/test.dart';
 
 void main() {
-  test('smoke', () {});
+  test('run global version', () {
+    final output = runExampleCli(
+      // Run outside of the example package directory so that the global
+      // version is used.
+      workingDirectory: '..',
+    );
+
+    expect(
+      output,
+      matches(
+        RegExp(
+          ".*Running global installation in Directory: '.*cli_launcher'.*",
+        ),
+      ),
+    );
+  });
+
+  test('run local version within package containing executable', () {
+    final output = runExampleCli(
+      workingDirectory: '.',
+    );
+
+    expect(
+      output,
+      matches(
+        RegExp(
+          ".*Running local installation in Directory: '.*example'.*",
+        ),
+      ),
+    );
+  });
+
+  test('run local version within consuming package', () {
+    final output = runExampleCli(workingDirectory: './consumer');
+
+    expect(
+      output,
+      matches(
+        RegExp(
+          ".*Running local installation in Directory: '.*consumer'.*",
+        ),
+      ),
+    );
+  });
+
+  test('run local version within sub directory of consuming package', () {
+    final dir = Directory('consumer/sub')..createSync(recursive: true);
+
+    final output = runExampleCli(workingDirectory: dir.path);
+
+    expect(
+      output,
+      matches(
+        RegExp(
+          ".*Running local installation in Directory: '.*sub'.*",
+        ),
+      ),
+    );
+  });
+}
+
+String runExampleCli({
+  List<String> arguments = const [],
+  required String workingDirectory,
+}) {
+  final result = Process.runSync(
+    'example',
+    arguments,
+    runInShell: true,
+    workingDirectory: workingDirectory,
+    stderrEncoding: utf8,
+    stdoutEncoding: utf8,
+  );
+
+  if (result.exitCode != 0) {
+    throw Exception(
+      'example CLI failed with exit code ${result.exitCode}:'
+      '\n${result.stdout}\n${result.stderr}',
+    );
+  }
+
+  return result.stdout as String;
 }
